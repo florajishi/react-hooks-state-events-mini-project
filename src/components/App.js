119ddfba5a27fa262e0724c4b340705bd157ec51
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-06-28 11:43:48 -0500

    pass down task data to TaskList component, map thru data

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+console.log(TASKS);
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [category, setCategory] = useState(CATEGORIES)
+
+  function handleDelete(event){
+
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter 
+        category={CATEGORIES}/>
       <NewTaskForm />
-      <TaskList />
+      <TaskList 
+        taskData={TASKS}
+        onTaskDelete={handleDelete}
+        />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ taskData }) {
+  const taskList = taskData.map(task => {
+    <Task
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      />
+  })
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
