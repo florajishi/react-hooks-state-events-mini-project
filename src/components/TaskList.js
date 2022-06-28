c761c56efc1a7231de64d229722b8d09103e97e2
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-06-28 13:05:21 -0500

    commit tree ffe3f184eece48970363a5075308d68f00e6089d

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Task from "./Task";
 
-function TaskList({ taskData, onTaskDelete }) {
-  const taskList = taskData.map(task => {
+function TaskList({ tasks, onTaskDelete }) {
+  const taskList = tasks.map(task => (
     <Task
       key={task.text}
       text={task.text}
       category={task.category}
       onTaskDelete={onTaskDelete}
       />
-  })
+  ))
   return (
     <div className="tasks">
       {taskList}
